cb5ef9b7a83e4468203408ed5d237974d7a94762
Ryan McEvoy <ryan.mcevoy@firebox.com>
2019-05-30 14:43:24 +0100

    Add more to the Abstract Gateway setup

--- Authenticator/Common/AbstractGateway.php
+++ Authenticator/Common/AbstractGateway.php
@@ -7,6 +7,7 @@ use GuzzleHttp\MessageFormatter;
 use GuzzleHttp\Middleware;
 use Psr\Log\LoggerInterface;
 use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 abstract class AbstractGateway
 {
@@ -14,6 +15,11 @@ abstract class AbstractGateway
      * @var LoggerInterface
      */
     protected $logger;
+
+    /**
+     * @var ParameterBag
+     */
+    protected $parameters;
 
     /**
      * @var Client
@@ -25,11 +31,34 @@ abstract class AbstractGateway
      */
     const DEBUG_FORMAT = ">>>>>>>>\n{method} {uri} HTTP/{version} {req_body}\n<<<<<<<<\nRESPONSE: {code} - {res_body}\n--------\n{error}\n";
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, $parameters = [])
     {
         $this->logger = $logger;
+        $this->parameters = new ParameterBag;
+
+        $this->initialiseParameters($parameters);
 
         $this->createDefaultHttpClient();
+    }
+
+    /**
+     * Initialise this gateway with default parameters
+     *
+     * @param  array $parameters
+     * @return AbstractGateway
+     */
+    public function initialiseParameters(array $parameters = [])
+    {
+        foreach ($parameters as $key => $value) {
+            $method = 'set'.ucfirst($this->camelCase($key));
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            } else {
+                $this->parameters->set($key, $value);
+            }
+        }
+
+        return $this;
     }
 
     /**
@@ -51,5 +80,22 @@ abstract class AbstractGateway
         $this->httpClient = new HttpClient([
             'handler' => $stack
         ]);
+    }
+
+    /**
+     * Convert a string to camelCase. Strings already in camelCase will not be harmed.
+     *
+     * @param  string  $str The input string
+     * @return string camelCased output string
+     */
+    protected function camelCase($str)
+    {
+        return preg_replace_callback(
+            '/_([a-z])/',
+            function ($match) {
+                return strtoupper($match[1]);
+            },
+            $str
+        );
     }
 }
