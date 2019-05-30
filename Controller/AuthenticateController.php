e17edc3f31e64a4df7dcb6938870baf25168614d
Ryan McEvoy <ryan.mcevoy@firebox.com>
2019-05-30 15:16:42 +0100

    Config and Controller dump of service

--- Controller/AuthenticateController.php
+++ Controller/AuthenticateController.php
@@ -1,13 +1,16 @@
 <?php
 namespace RmAuthenticatorBundle\Controller;
 
+use RmAuthenticatorBundle\Authenticator\Factory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
 class AuthenticateController extends AbstractController
 {
-    public function index(Request $request)
+    public function index(Request $request, Factory $factory)
     {
+        dump($factory);
+
         return $this->render('@RmAuthenticatorBundle/index.html.twig');
     }
 }
